4ac99152022a01bfc1501304c99dcf9082d057ce
Nmuta Jones <nmuta.jones@charter.com>
2020-03-03 13:40:36 -0700

    Add delete and update methods.

--- src/flights/flights.service.ts
+++ src/flights/flights.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, UpdateResult } from 'typeorm';
 import { Flights } from './flights.entity';
+import { Flight } from './flight.model';
 
 @Injectable()
 export class FlightsService {
@@ -10,11 +11,32 @@ export class FlightsService {
     private readonly flightRepository: Repository<Flights>,
   ) {}
 
-  async findAll(): Promise<Flights[]> {
-    return this.flightRepository.find();
+  async create(flight: Flight): Promise<any> {
+    return await this.flightRepository.save(flight);
   }
 
   async findOne(id: number): Promise<any> {
     return this.flightRepository.findOne(id);
   }
+
+  async findAll(): Promise<Flights[]> {
+    return this.flightRepository.find();
+  }
+
+  async query(orig: string, dest: string): Promise<any> {
+    return await this.flightRepository.query(
+      ` Select * from flights where 
+       origin = '${orig}' and destination = '${dest}' 
+    `);
+  }
+
+  async update(flight: Flight): Promise<UpdateResult> {
+    return await this.flightRepository.update(flight.id, flight);
+  }
+  
+
+  async delete(id: number): Promise<any> {
+    return this.flightRepository.delete(id);
+  }
+
 }
